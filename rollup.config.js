9fdf687c4be3dbddf31ceb73e88818c03c89e7cf
alhacen <alhaqhassan@gmail.com>
2025-04-25 14:01:52 +0530

    feat: install tailwind

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,13 @@ import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import terser from "@rollup/plugin-terser";
 import typescript from "rollup-plugin-typescript2"; // For Typescript
+import postcss from "rollup-plugin-postcss";
+import tailwindcss from "tailwindcss";
+import tailwindConfig from "./tailwind.config.cjs";
+import commonjs from "@rollup/plugin-commonjs";
 export default [
   {
-    input: "./src/index.ts",
+    input: "./src/index.tsx",
     output: [
       {
         file: "dist/index.js",
@@ -18,6 +22,18 @@ export default [
       },
     ],
     plugins: [
+      commonjs(),
+      postcss({
+        config: {
+          path: "./postcss.config.js",
+        },
+        extensions: [".css"],
+        minimize: true,
+        inject: {
+          insertAt: "top",
+        },
+        plugins: [tailwindcss(tailwindConfig)],
+      }),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
