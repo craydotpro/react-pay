9084306df802116f3d54f52701a2c5ce4417482e
alhacen <alhaqhassan@gmail.com>
2025-05-12 18:23:24 +0530

    feat: added arb and base mainnet chains

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,13 @@
 import {
+  arbitrum,
   arbitrumSepolia,
+  base,
   baseSepolia,
+  mainnet,
+  optimism,
   optimismSepolia,
+  polygon,
+  polygonAmoy,
   sepolia,
 } from "viem/chains";
 import { QueryClient } from "@tanstack/react-query";
@@ -16,4 +22,10 @@ export const CHAINS = {
   11_155_111: sepolia,
   421_614: arbitrumSepolia,
   11155420: optimismSepolia,
+  80_002: polygonAmoy,
+  8453: base,
+  42_161: arbitrum,
+  1: mainnet,
+  10: optimism,
+  137: polygon,
 } as any;
